537f0f7c87255f4580c2bb4e2727c5eef12d6b3e
sazzh <100480867+sazzh@users.noreply.github.com>
2026-01-01 21:28:34 +1100

    Add db connection confirmation

--- db.js
+++ db.js
@@ -2,10 +2,16 @@ import pg from 'pg';
 
 // Create a new pool instance to manage PostgreSQL connections
 // Pool creates and resuses multiple connections automatically
-export const db = new pg.Pool({
+const db = new pg.Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
 });
+
+db.on("connect", () => {
+    console.log("Connected to the database");
+});
+
+export default db;
